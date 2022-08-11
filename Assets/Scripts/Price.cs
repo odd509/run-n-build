f171524800b5872ed5e67177296d6680ea624bd1
b2200356018 <gozlu.oguz@gmail.com>
2022-08-11 21:09:18 +0300

    prices n such

---
+++ Assets/Scripts/Price.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Price : MonoBehaviour
+{
+
+    public float price;
+    public TextMeshProUGUI priceText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        priceText.text = price.ToString() + "$";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
